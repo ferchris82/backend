dd9ad714b3c75eea292a0de5baa73f4251e69b2b
ChrisferDev <ferchris82@hotmail.com>
2024-06-09 14:20:12 -0500

    Versión 1.0

--- src/main/java/com/chrisferdev/ecommerce/backend/infrastructure/entity/CategoryEntity.java
+++ src/main/java/com/chrisferdev/ecommerce/backend/infrastructure/entity/CategoryEntity.java
@@ -9,6 +9,9 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import java.time.LocalDateTime;
 
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
 
 @Entity
 @Table(name = "categories")
@@ -19,7 +22,9 @@ public class CategoryEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String name;
+    @CreationTimestamp
     private LocalDateTime dateCreated;
+    @UpdateTimestamp
     private LocalDateTime dateUpdated;
     
     
