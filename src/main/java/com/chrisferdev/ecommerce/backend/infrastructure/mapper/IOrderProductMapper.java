611d6de2057a00af9aa3d1b92d5c93af6e730d3d
ChrisferDev <ferchris82@hotmail.com>
2024-06-10 16:15:32 -0500

    Versión 1.0

--- src/main/java/com/chrisferdev/ecommerce/backend/infrastructure/mapper/IOrderProductMapper.java
+++ src/main/java/com/chrisferdev/ecommerce/backend/infrastructure/mapper/IOrderProductMapper.java
@@ -1,2 +1,25 @@
-package com.chrisferdev.ecommerce.backend.infrastructure.mapper;public interface IOrderProductMapper {
+package com.chrisferdev.ecommerce.backend.infrastructure.mapper;
+
+import com.chrisferdev.ecommerce.backend.domain.model.OrderProduct;
+import com.chrisferdev.ecommerce.backend.infrastructure.entity.OrderProductEntity;
+import org.mapstruct.InheritInverseConfiguration;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Mappings;
+
+@Mapper(componentModel = "spring")
+public interface IOrderProductMapper {
+    @Mappings(
+            {
+                    @Mapping(source = "id", target = "id"),
+                    @Mapping(source = "quantity", target = "quantity"),
+                    @Mapping(source = "price", target = "price"),
+                    @Mapping(source = "productId", target = "productId"),
+            }
+    )
+    OrderProduct toOrderProduct(OrderProductEntity orderProductEntity);
+    Iterable<OrderProduct> toOrderProductList(Iterable<OrderProductEntity> orderProductEntities);
+
+    @InheritInverseConfiguration
+    OrderProductEntity toOrderProductEntity(OrderProduct orderProduct);
 }
