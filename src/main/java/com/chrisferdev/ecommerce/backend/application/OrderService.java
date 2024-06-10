7ce7f0e6c004b16ec3de41b4c7477bc22bee7ede
ChrisferDev <ferchris82@hotmail.com>
2024-06-10 15:49:27 -0500

    Versión 1.0

---
+++ src/main/java/com/chrisferdev/ecommerce/backend/application/OrderService.java
@@ -0,0 +1,26 @@
+package com.chrisferdev.ecommerce.backend.application;
+
+import com.chrisferdev.ecommerce.backend.domain.model.Order;
+import com.chrisferdev.ecommerce.backend.domain.port.IOrderRepository;
+
+public class OrderService {
+
+    private final IOrderRepository iOrderRepository;
+
+    public OrderService(IOrderRepository iOrderRepository) {
+        this.iOrderRepository = iOrderRepository;
+    }
+
+    public Order save(Order order){
+        return this.iOrderRepository.save(order);
+    }
+    public Iterable<Order> findAll(){
+        return this.iOrderRepository.findAll();
+    }
+    public Iterable<Order> findByUserId(Integer userId){
+        return this.iOrderRepository.findByUserId(userId);
+    }
+    public void updateStateById(Integer id, String state){
+        this.iOrderRepository.updateStateById(id, state);
+    }
+}
---
+++ src/main/java/com/chrisferdev/ecommerce/backend/domain/port/IOrderRepository.java
@@ -0,0 +1,12 @@
+package com.chrisferdev.ecommerce.backend.domain.port;
+
+import com.chrisferdev.ecommerce.backend.domain.model.Order;
+
+public interface IOrderRepository {
+
+    Order save(Order order);
+    Order findById(Integer id);
+    Iterable<Order> findAll();
+    Iterable<Order> findByUserId(Integer userId);
+    void updateStateById(Integer id, String state);
+}
